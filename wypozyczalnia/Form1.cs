1578a39ae17b5f4df06f14b41c9cd313f82106bb
Łukasz Byrka <lukasz.byrka1@gmail.com>
2019-12-01 13:27:08 +0100

    test

--- wypozyczalnia/Form1.cs
+++ wypozyczalnia/Form1.cs
@@ -16,5 +16,10 @@ namespace wypozyczalnia
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
